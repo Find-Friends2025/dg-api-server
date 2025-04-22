6efff757e618b280ad8cf40eb8e365464491dcca
최미래 <miraechoi@dgsw.hs.kr>
2025-04-22 11:27:14 +0900

    feature: add user not found custom exception

---
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/account/exception/UserNotFoundException.kt
@@ -0,0 +1,7 @@
+package com.oneplace.dgapiserver.domain.account.exception
+
+import com.oneplace.dgapiserver.global.error.BusinessException
+import com.oneplace.dgapiserver.global.error.ErrorCode
+
+class UserNotFoundException(val uid: String) : BusinessException(ErrorCode.USER_NOT_FOUND)
+
