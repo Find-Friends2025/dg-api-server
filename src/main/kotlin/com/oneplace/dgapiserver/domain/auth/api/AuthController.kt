c1ff998baebe542b928b329246b11a943174aed8
최미래 <miraechoi@dgsw.hs.kr>
2025-04-28 18:09:13 +0900

    feature: add auth /register api

--- src/main/kotlin/com/oneplace/dgapiserver/domain/auth/api/AuthController.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/auth/api/AuthController.kt
@@ -1,6 +1,7 @@
 package com.oneplace.dgapiserver.domain.auth.api
 
 import com.oneplace.dgapiserver.domain.account.application.AccountService
+import com.oneplace.dgapiserver.domain.auth.api.dto.request.RegisterRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,5 +15,10 @@ class AuthController(
     fun login(@RequestHeader("Authorization") authHeader: String): ResponseEntity<Any> {
         val token = authHeader.removePrefix("Bearer ").trim()
         return accountService.handleFirebaseLogin(token)
+    }
+
+    @PostMapping("/register")
+    fun signup(@RequestBody registerRequest: RegisterRequest): ResponseEntity<Unit> {
+//        return accountService.registerUser(registerUserRequest)
     }
 }
