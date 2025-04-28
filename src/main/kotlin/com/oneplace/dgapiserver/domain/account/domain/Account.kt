5a515eb9cd20321ff723b6c2d807a5af91b2dcec
최미래 <miraechoi@dgsw.hs.kr>
2025-04-28 20:25:10 +0900

    feature: add columns to account

--- src/main/kotlin/com/oneplace/dgapiserver/domain/account/domain/Account.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/account/domain/Account.kt
@@ -1,6 +1,7 @@
 package com.oneplace.dgapiserver.domain.account.domain
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
 @Table(name = "account")
@@ -10,6 +11,20 @@ class Account(
     val id: Long = 0,
 
     @Column(nullable = false, unique = true)
-    val uid: String
+    val uid: String,
 
+    @Column(nullable = true)
+    val gender: String,
+
+    @Column(nullable = true)
+    val birth: LocalDate,
+
+    @Column(nullable = true)
+    val location: String,
+
+    @Column(nullable = true)
+    val nickname: String,
+
+    @Column(nullable = true, columnDefinition = "TEXT")
+    val profilePicUrl: String
 )
