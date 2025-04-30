ef5230a0aa356aa58a7e32923cf281a62bea574b
최미래 <miraechoi@dgsw.hs.kr>
2025-04-30 21:38:10 +0900

    refactor: refactor account error

--- src/main/kotlin/com/oneplace/dgapiserver/global/error/account/AccountError.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/global/error/account/AccountError.kt
@@ -7,8 +7,7 @@ enum class AccountError(
     val status: HttpStatus
 ) {
     BAD_REQUEST("Bad Request", HttpStatus.BAD_REQUEST),
-    INTERNAL_SERVER_ERROR("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR),
-
     INVALID_FIREBASE_TOKEN("Invalid Firebase token", HttpStatus.UNAUTHORIZED),
-    USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND)
+    USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS("This user has already completed registration", HttpStatus.CONFLICT)
 }
