461b0d0b2835023fe3a040df825d2b32ff9ea077
최미래 <miraechoi@dgsw.hs.kr>
2025-04-30 21:36:09 +0900

    feature: add uid value to register request dto

--- src/main/kotlin/com/oneplace/dgapiserver/domain/auth/api/dto/request/RegisterRequest.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/auth/api/dto/request/RegisterRequest.kt
@@ -3,6 +3,7 @@ package com.oneplace.dgapiserver.domain.auth.api.dto.request
 import java.time.LocalDate
 
 data class RegisterRequest(
+    val uid: String,
     val gender: String = "",
     val birth: LocalDate,
     val location: String = "",
