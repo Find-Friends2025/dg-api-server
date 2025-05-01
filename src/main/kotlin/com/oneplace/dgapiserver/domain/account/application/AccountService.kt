4dfa856b5bdff28cad6c8c941065b8a9c6531afb
최미래 <miraechoi@dgsw.hs.kr>
2025-05-01 23:52:20 +0900

    feature: add register user service logic

--- src/main/kotlin/com/oneplace/dgapiserver/domain/account/application/AccountService.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/account/application/AccountService.kt
@@ -6,5 +6,5 @@ import org.springframework.http.ResponseEntity
 interface AccountService {
     fun existsByUid(uid: String): Boolean
     fun handleFirebaseLogin(idToken: String): ResponseEntity<Any>
-    fun registerUser(registerRequest: RegisterRequest): ResponseEntity<Unit>
+    fun registerUser(registerRequest: RegisterRequest): ResponseEntity<Any>
 }
--- src/main/kotlin/com/oneplace/dgapiserver/domain/account/application/AccountServiceImpl.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/account/application/AccountServiceImpl.kt
@@ -1,11 +1,12 @@
 package com.oneplace.dgapiserver.domain.account.application
 
-import com.oneplace.dgapiserver.domain.account.domain.Account
 import com.oneplace.dgapiserver.domain.account.domain.repository.AccountRepository
 import com.oneplace.dgapiserver.domain.account.exception.InvalidFirebaseTokenException
+import com.oneplace.dgapiserver.domain.account.exception.UserAlreadyExistsException
 import com.oneplace.dgapiserver.domain.account.exception.UserNotFoundException
 import com.oneplace.dgapiserver.domain.auth.api.dto.request.RegisterRequest
 import com.oneplace.dgapiserver.global.firebase.FirebaseTokenVerifier
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
@@ -14,6 +15,7 @@ class AccountServiceImpl(
     private val firebaseTokenVerifier: FirebaseTokenVerifier,
     private val accountRepository: AccountRepository
 ) : AccountService {
+
     override fun existsByUid(uid: String): Boolean {
         return accountRepository.existsByUid(uid)
     }
@@ -32,18 +34,25 @@ class AccountServiceImpl(
         return ResponseEntity.ok(mapOf("message" to "Login Success"))
     }
 
-    override fun registerUser(registerRequest: RegisterRequest): ResponseEntity<Unit> {
-        val account = Account(
-            uid = "",
-            gender = registerRequest.gender,
-            birth = registerRequest.birth,
-            location = registerRequest.location,
-            nickname = registerRequest.nickname,
-            profilePicUrl = registerRequest.profilePicUrl
-        )
+    override fun registerUser(registerRequest: RegisterRequest): ResponseEntity<Any> {
+        val account = accountRepository.findByUid(registerRequest.uid)
+            ?: throw UserNotFoundException()
+
+        if (account.isRegistered) {
+            throw UserAlreadyExistsException()
+        }
+
+        account.gender = registerRequest.gender
+        account.birth = registerRequest.birth
+        account.location = registerRequest.location
+        account.nickname = registerRequest.nickname
+        account.profilePicUrl = registerRequest.profilePicUrl
+        account.isRegistered = true
+
         accountRepository.save(account)
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(mapOf("message" to "Registration Success"))
     }
 
 }
