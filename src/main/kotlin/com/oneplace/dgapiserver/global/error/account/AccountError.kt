f1f4a69e466170f26650ed0315b7cb59d6cdc0af
최미래 <miraechoi@dgsw.hs.kr>
2025-05-12 19:47:35 +0900

    fix: change account error response to korean

--- src/main/kotlin/com/oneplace/dgapiserver/global/error/account/AccountError.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/global/error/account/AccountError.kt
@@ -6,8 +6,8 @@ enum class AccountError(
     val message: String,
     val status: HttpStatus
 ) {
-    BAD_REQUEST("Bad Request", HttpStatus.BAD_REQUEST),
-    INVALID_FIREBASE_TOKEN("Invalid Firebase token", HttpStatus.UNAUTHORIZED),
-    USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND),
-    USER_ALREADY_EXISTS("This user has already completed registration", HttpStatus.CONFLICT)
+    BAD_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_FIREBASE_TOKEN("유효하지 않은 Firebase 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    USER_NOT_FOUND("Firebase 인증은 되었지만, 회원 정보가 없습니다.", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS("이미 회원가입이 완료된 회원입니다.", HttpStatus.CONFLICT)
 }
