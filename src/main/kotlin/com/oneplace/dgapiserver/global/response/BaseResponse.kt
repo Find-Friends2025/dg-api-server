2455e7b4f538a682b790d180ad7de22a907d8ee9
최미래 <miraechoi@dgsw.hs.kr>
2025-05-12 21:37:15 +0900

    feature: add global base response

---
+++ src/main/kotlin/com/oneplace/dgapiserver/global/response/BaseResponse.kt
@@ -0,0 +1,26 @@
+package com.oneplace.dgapiserver.global.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.oneplace.dgapiserver.global.error.CustomErrorCode
+import org.springframework.http.HttpStatus
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class BaseResponse<T>(
+
+    override val status: Int = HttpStatus.OK.value(),
+    override val success: Boolean = true,
+    override val state: String = "OK",
+    override val message: String,
+    val data: T? = null,
+
+    ) : ResponseInterface {
+
+    // errorResponse constructor
+    constructor(code: CustomErrorCode) : this(
+        status = code.status.value(),
+        success = false,
+        state = code.state,
+        message = code.message
+    )
+
+}
