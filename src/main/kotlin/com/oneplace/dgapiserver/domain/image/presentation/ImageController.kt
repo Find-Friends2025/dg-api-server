01b9e96b7971d4dbd6789dbf1dd13f9721b7ab2c
최미래 <miraechoi@dgsw.hs.kr>
2025-05-12 21:46:35 +0900

    refactor: change response entity to base response

--- src/main/kotlin/com/oneplace/dgapiserver/domain/image/presentation/ImageController.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/image/presentation/ImageController.kt
@@ -2,8 +2,7 @@ package com.oneplace.dgapiserver.domain.image.presentation
 
 import com.oneplace.dgapiserver.domain.image.application.ImageService
 import com.oneplace.dgapiserver.domain.image.application.dto.response.UrlResponse
-import lombok.RequiredArgsConstructor
-import org.springframework.http.ResponseEntity
+import com.oneplace.dgapiserver.global.response.BaseResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
@@ -12,14 +11,18 @@ import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/image")
-@RequiredArgsConstructor
 class ImageController(
     private val imageService: ImageService
 ) {
 
     @PostMapping("/upload")
-    fun uploadImage(@RequestPart("image") multipartFile: MultipartFile?): ResponseEntity<UrlResponse> {
-        return ResponseEntity.ok().body(imageService.uploadImage(multipartFile))
+    fun uploadImage(
+        @RequestPart("image") multipartFile: MultipartFile?
+    ): BaseResponse<UrlResponse> {
+        return BaseResponse(
+            message = "Image uploaded successfully",
+            data = imageService.uploadImage(multipartFile)
+        )
     }
-
 }
+
