566a5fcffb29b3730b57f957b3805e246a910915
최미래 <miraechoi@dgsw.hs.kr>
2025-05-22 23:11:25 +0900

    feature: add find by uid to repository

--- src/main/kotlin/com/oneplace/dgapiserver/domain/account/domain/repository/AccountRepository.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/account/domain/repository/AccountRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface AccountRepository : JpaRepository<Account, Long> {
     fun existsByUid(uid: String): Boolean
+    fun findByUid(uid: String): Account?
 }
