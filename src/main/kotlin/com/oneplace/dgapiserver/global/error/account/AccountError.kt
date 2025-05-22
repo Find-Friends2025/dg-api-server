ce7c50680374fbccee2f705378e9ff56f1d3cd85
최미래 <miraechoi@dgsw.hs.kr>
2025-05-22 23:13:31 +0900

    feature: add refresh not found error

--- src/main/kotlin/com/oneplace/dgapiserver/global/error/account/AccountError.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/global/error/account/AccountError.kt
@@ -9,5 +9,6 @@ enum class AccountError(
     BAD_REQUEST("Invalid request", HttpStatus.BAD_REQUEST),
     INVALID_FIREBASE_TOKEN("Invalid Firebase token", HttpStatus.UNAUTHORIZED),
     USER_NOT_FOUND("Firebase authentication is successful, but there is no member information", HttpStatus.NOT_FOUND),
+    REFRESH_NOT_FOUND("Refresh token is not found", HttpStatus.NOT_FOUND),
     USER_ALREADY_EXISTS("You are already a registered member", HttpStatus.CONFLICT)
 }
