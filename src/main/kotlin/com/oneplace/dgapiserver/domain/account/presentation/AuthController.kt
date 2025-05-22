b98e936ea83e3536947330ee9c6ba3517d09b134
최미래 <miraechoi@dgsw.hs.kr>
2025-05-22 23:13:09 +0900

    refactor: fix auth controller apis

---
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/account/presentation/AuthController.kt
@@ -0,0 +1,35 @@
+package com.oneplace.dgapiserver.domain.account.presentation
+
+import com.oneplace.dgapiserver.domain.account.application.AuthService
+import com.oneplace.dgapiserver.domain.account.application.dto.AuthLoginDto
+import com.oneplace.dgapiserver.domain.account.application.dto.AuthTokenDto
+import com.oneplace.dgapiserver.domain.account.application.dto.RegisterReqDto
+import com.oneplace.dgapiserver.global.response.BaseResponse
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/auth")
+class AuthController(
+    private val authService: AuthService
+) {
+
+    @PostMapping("/login")
+    fun login(
+        @RequestHeader("Authorization") authHeader: String
+    ): BaseResponse<AuthLoginDto> {
+        return BaseResponse(
+            message = "Login Success",
+            data = authService.login(authHeader)
+        )
+    }
+
+    @PostMapping("/register")
+    fun signup(
+        @RequestBody registerRequest: RegisterReqDto
+    ): BaseResponse<AuthTokenDto> {
+        return BaseResponse(
+            message = "Registration Success",
+            data = authService.register(registerRequest)
+        )
+    }
+}
