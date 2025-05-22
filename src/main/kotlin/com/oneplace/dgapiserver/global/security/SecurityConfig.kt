baf15b155ec69e1cd2743a94b4b3490a60601714
최미래 <miraechoi@dgsw.hs.kr>
2025-05-22 23:24:35 +0900

    feature: add security config

---
+++ src/main/kotlin/com/oneplace/dgapiserver/global/security/SecurityConfig.kt
@@ -0,0 +1,82 @@
+package com.oneplace.dgapiserver.global.security
+
+import com.oneplace.dgapiserver.global.filter.AuthenticationFilter
+import com.oneplace.dgapiserver.global.filter.LoggingFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class SecurityConfig(
+    private val customAccessDeniedHandler: CustomAccessDeniedHandler,
+    private val customAuthenticationEntryPointHandler: CustomAuthenticationEntryPointHandler,
+    private val authenticationFilter: AuthenticationFilter,
+    private val loggingFilter: LoggingFilter,
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.formLogin { it.disable() }
+            .httpBasic { it.disable() }
+
+        http.csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
+
+        http.exceptionHandling { handling ->
+            handling.accessDeniedHandler(customAccessDeniedHandler)
+            handling.authenticationEntryPoint(customAuthenticationEntryPointHandler)
+        }
+
+        http.sessionManagement { sessionManagement ->
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }
+
+        http.addFilterBefore(loggingFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(authenticationFilter, LoggingFilter::class.java)
+
+        http.authorizeHttpRequests { httpRequests ->
+            httpRequests
+
+                // auth
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+
+                .anyRequest().denyAll()
+        }
+
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowedMethods = listOf(
+            HttpMethod.GET.name(),
+            HttpMethod.POST.name(),
+            HttpMethod.PUT.name(),
+            HttpMethod.PATCH.name(),
+            HttpMethod.DELETE.name(),
+            HttpMethod.OPTIONS.name()
+        )
+
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+}
