b7275d9b1f6f19d27ca2f2df342b7af804036a9f
최미래 <miraechoi@dgsw.hs.kr>
2025-05-29 13:52:54 +0900

    feature: add user like repository

---
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/like/persistance/repository/UserLikeRepository.kt
@@ -0,0 +1,11 @@
+package com.oneplace.dgapiserver.domain.like.persistance.repository
+
+import com.oneplace.dgapiserver.domain.like.persistance.UserLike
+import com.oneplace.dgapiserver.domain.user.persistance.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserLikeRepository : JpaRepository<UserLike, Long> {
+    fun existsByFromUserAndToUser(fromUser: User, toUser: User): Boolean
+    fun deleteByFromUserAndToUser(fromUser: User, toUser: User): Long
+    fun countByToUser(toUser: User): Long
+}
