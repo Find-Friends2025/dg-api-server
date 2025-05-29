6dc8382e0496d0de93c0a58f3803ce9228ee94f1
최미래 <miraechoi@dgsw.hs.kr>
2025-05-29 17:46:01 +0900

    refactor: add uid validation when signup

--- src/main/kotlin/com/oneplace/dgapiserver/domain/auth/application/AuthService.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/auth/application/AuthService.kt
@@ -7,5 +7,5 @@ import com.oneplace.dgapiserver.domain.auth.application.dto.RegisterReqDto
 interface AuthService {
     fun login(idToken: String): AuthLoginDto
     fun refresh(token: String): AuthTokenDto
-    fun register(registerRequest: RegisterReqDto): AuthTokenDto
+    fun register(idToken: String, request: RegisterReqDto): AuthTokenDto
 }
