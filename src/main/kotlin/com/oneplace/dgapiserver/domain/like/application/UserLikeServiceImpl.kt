7cd6a8180f0dc968d7943ce041d677af70d0f615
최미래 <miraechoi@dgsw.hs.kr>
2025-06-04 19:56:33 +0900

    refactor: remove comments in user like serviceimpl

--- src/main/kotlin/com/oneplace/dgapiserver/domain/like/application/UserLikeServiceImpl.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/like/application/UserLikeServiceImpl.kt
@@ -17,7 +17,6 @@ class UserLikeServiceImpl (
     private val userAuthenticationHolder: UserAuthenticationHolder
 ) : UserLikeService {
 
-    // 좋아요 누르기
     @Transactional
     override fun like(toUserId: Long) {
         val fromUser = userAuthenticationHolder.current()
@@ -34,7 +33,6 @@ class UserLikeServiceImpl (
         userLikeRepository.save(like)
     }
 
-    // 좋아요 취소
     @Transactional
     override fun unlike(toUserId: Long) {
         val fromUser = userAuthenticationHolder.current()
@@ -44,17 +42,15 @@ class UserLikeServiceImpl (
         if (deleted == 0L) throw IllegalStateException("좋아요 기록이 없습니다.")
     }
 
-    // 좋아요 여부 확인
+    override fun countLikes(toUserId: Long): Long {
+        val toUser = userRepository.getReferenceById(toUserId)
+        return userLikeRepository.countByToUser(toUser)
+    }
+
     override fun isLiked(toUserId: Long): Boolean {
         val fromUser = userAuthenticationHolder.current()
         val toUser = userRepository.getReferenceById(toUserId)
         return userLikeRepository.existsByFromUserAndToUser(fromUser, toUser)
-    }
-
-    // 특정 유저가 받은 좋아요 수
-    override fun countLikes(toUserId: Long): Long {
-        val toUser = userRepository.getReferenceById(toUserId)
-        return userLikeRepository.countByToUser(toUser)
     }
 
     override fun getUsersWhoLikedMe(): List<WhoLikedMeResponse> {
