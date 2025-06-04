4385e03dbe978701c3120568de54e20b68f10bb2
최미래 <miraechoi@dgsw.hs.kr>
2025-06-04 19:38:02 +0900

    feature: add get user who liked me api

--- src/main/kotlin/com/oneplace/dgapiserver/domain/like/presentation/LikeController.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/like/presentation/LikeController.kt
@@ -1,6 +1,8 @@
 package com.oneplace.dgapiserver.domain.like.presentation
 
 import com.oneplace.dgapiserver.domain.like.application.UserLikeService
+import com.oneplace.dgapiserver.domain.like.application.dto.response.WhoLikedMeResponse
+import com.oneplace.dgapiserver.global.response.BaseResponse
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,23 +11,31 @@ class LikeController (
     private val userLikeService: UserLikeService
 ) {
 
-    @PostMapping("/{toUserId}/like")
+    @PostMapping("/{toUserId}")
     fun like(@PathVariable toUserId: Long) {
         userLikeService.like(toUserId)
     }
 
-    @DeleteMapping("/{toUserId}/like")
+    @DeleteMapping("/{toUserId}")
     fun unlike(@PathVariable toUserId: Long) {
         userLikeService.unlike(toUserId)
     }
 
-    @GetMapping("/{toUserId}/like/count")
+    @GetMapping("/{toUserId}/count")
     fun countLikes(@PathVariable toUserId: Long): Long {
         return userLikeService.countLikes(toUserId)
     }
 
-    @GetMapping("/{toUserId}/like/status")
+    @GetMapping("/{toUserId}/status")
     fun isLiked(@PathVariable toUserId: Long): Boolean {
         return userLikeService.isLiked(toUserId)
     }
+
+    @GetMapping("/me/likes")
+    fun getUsersWhoLikedMe(): BaseResponse<List<WhoLikedMeResponse>> =
+        BaseResponse(
+            message = "Users who liked me retrieved successfully",
+            data = userLikeService.getUsersWhoLikedMe()
+        )
+
 }
