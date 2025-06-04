8bcfb744baf8c6316ee1fa328ff6076b9bab19ca
최미래 <miraechoi@dgsw.hs.kr>
2025-06-04 19:37:26 +0900

    feature: add findAllByToUser and findAllByFromUser

--- src/main/kotlin/com/oneplace/dgapiserver/domain/like/persistance/repository/UserLikeRepository.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/like/persistance/repository/UserLikeRepository.kt
@@ -8,4 +8,6 @@ interface UserLikeRepository : JpaRepository<UserLike, Long> {
     fun existsByFromUserAndToUser(fromUser: User, toUser: User): Boolean
     fun deleteByFromUserAndToUser(fromUser: User, toUser: User): Long
     fun countByToUser(toUser: User): Long
+    fun findAllByToUser(toUser: User): List<UserLike>
+    fun findAllByFromUser(fromUser: User): List<UserLike>
 }
