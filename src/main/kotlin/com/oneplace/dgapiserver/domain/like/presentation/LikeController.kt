418d42202ce7a87accc4b6e96ff69f56105d1c73
최미래 <miraechoi@dgsw.hs.kr>
2025-06-04 19:55:31 +0900

    refactor: refactor like controller code style

--- src/main/kotlin/com/oneplace/dgapiserver/domain/like/presentation/LikeController.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/like/presentation/LikeController.kt
@@ -12,30 +12,51 @@ class LikeController (
 ) {
 
     @PostMapping("/{toUserId}")
-    fun like(@PathVariable toUserId: Long) {
-        userLikeService.like(toUserId)
+    fun like(
+        @PathVariable toUserId: Long
+    ): BaseResponse<Unit> {
+        return BaseResponse(
+            message = "Like Success",
+            data = userLikeService.like(toUserId)
+        )
     }
 
     @DeleteMapping("/{toUserId}")
-    fun unlike(@PathVariable toUserId: Long) {
-        userLikeService.unlike(toUserId)
+    fun unlike(
+        @PathVariable toUserId: Long
+    ): BaseResponse<Unit> {
+        return BaseResponse(
+            message = "Like Cancel Success",
+            data = userLikeService.unlike(toUserId)
+        )
     }
 
     @GetMapping("/{toUserId}/count")
-    fun countLikes(@PathVariable toUserId: Long): Long {
-        return userLikeService.countLikes(toUserId)
+    fun countLikes(
+        @PathVariable toUserId: Long
+    ): BaseResponse<Long> {
+        return BaseResponse(
+            message = "Successfully checked the number of likes received by the user",
+            data = userLikeService.countLikes(toUserId)
+        )
     }
 
     @GetMapping("/{toUserId}/status")
-    fun isLiked(@PathVariable toUserId: Long): Boolean {
-        return userLikeService.isLiked(toUserId)
+    fun isLiked(
+        @PathVariable toUserId: Long
+    ): BaseResponse<Boolean> {
+        return BaseResponse(
+            message = "Successful confirmation of likes",
+            data = userLikeService.isLiked(toUserId)
+        )
     }
 
     @GetMapping("/me/likes")
-    fun getUsersWhoLikedMe(): BaseResponse<List<WhoLikedMeResponse>> =
-        BaseResponse(
+    fun getUsersWhoLikedMe()
+    : BaseResponse<List<WhoLikedMeResponse>> {
+        return BaseResponse(
             message = "Users who liked me retrieved successfully",
             data = userLikeService.getUsersWhoLikedMe()
         )
-
+    }
 }
