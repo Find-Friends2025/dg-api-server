5e14ca27b5d28c52bb3802d32e6d3626060c9118
최미래 <miraechoi@dgsw.hs.kr>
2025-06-04 19:37:52 +0900

    feature: add get user who liked me service logic

--- src/main/kotlin/com/oneplace/dgapiserver/domain/like/application/UserLikeService.kt
+++ src/main/kotlin/com/oneplace/dgapiserver/domain/like/application/UserLikeService.kt
@@ -1,8 +1,12 @@
 package com.oneplace.dgapiserver.domain.like.application
 
+import com.oneplace.dgapiserver.domain.like.application.dto.response.WhoLikedMeResponse
+
 interface UserLikeService {
     fun like(toUserId: Long)
     fun unlike(toUserId: Long)
     fun isLiked(toUserId: Long): Boolean
     fun countLikes(toUserId: Long): Long
+    fun getUsersWhoLikedMe(): List<WhoLikedMeResponse>
+
 }
